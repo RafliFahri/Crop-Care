e71ee78d06041f3b754220100f49eebeede89651
riskaaxpps <riska7amelia@gmail.com>
2024-12-02 16:14:01 +0700

    frontend riska

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {};
+=======
+const nextConfig = {
+  reactStrictMode: true,
+};
+>>>>>>> f9fd571 (Initial commit from Create Next App)
 
 export default nextConfig;
